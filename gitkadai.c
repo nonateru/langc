3a5215acd2fab3db075ad732d7dd850d69cfe847
nonateru <148518682+nonateru@users.noreply.github.com>
2023-11-17 14:33:32 +0900

    Add files via upload

---
+++ gitkadai.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+    int x,y,z,price;
+    float w;
+    printf("Welcome to Programming!\n");
+    x=10;
+    y=x*2;
+    z=x+y;
+    printf("x=%d y=%d z=%d\n",x,y,z);
+    price=500;
+    w=price*1.1;
+    printf("w=%0.1f\n",w);
+    return 0;
+}
